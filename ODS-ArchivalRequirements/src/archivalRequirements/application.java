1e8625950d6a6bbbd44a4a6dde0ab2205becd971
Asbjoedt <asbjoern.skoedt@gmail.com>
2022-09-11 20:54:17 +0200

    Commit

--- ODS-ArchivalRequirements/src/archivalRequirements/application.java
+++ ODS-ArchivalRequirements/src/archivalRequirements/application.java
@@ -6,20 +6,32 @@ public class application {
 		
 		String function = args[0];
 		String filepath = args[1];
+		String extension = filepath.substring(filepath.lastIndexOf('.') + 1);
 		System.out.println(function + " filepath " + filepath);
 		
-		// CELL VALUES
-		cellValues cellValue = new cellValues();
-		Boolean hasCellValue = cellValue.check(filepath);
-		if (hasCellValue == false) {
-
-		}
+		switch(extension.toLowerCase()) {
 		
-		// DATA CONNECTIONS
-		dataConnections dataConnection = new dataConnections();
-		int conns = dataConnection.check(filepath);
-		if (function == "CheckChange" && conns > 0) {
-			dataConnection.change(filepath);
+			case "fods":
+			case "ods":
+			case "ots":
+				// CELL VALUES
+				cellValues cellValue = new cellValues();
+				Boolean hasCellValue = cellValue.check(filepath);
+				if (hasCellValue == false) {
+
+				}
+			
+				// DATA CONNECTIONS
+				dataConnections dataConnection = new dataConnections();
+				int conns = dataConnection.check(filepath);
+				if (function == "CheckChange" && conns > 0) {
+					dataConnection.change(filepath);
+				}
+				
+				break;
+				
+				default:
+					System.out.println("--> File format is not an accepted extension");
 		}
 	}
 }
